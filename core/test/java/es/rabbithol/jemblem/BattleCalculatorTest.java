881f2c239b6882f1ee2721c772c6859b90b7201d
Kevin Most <kevin@kevinmost.com>
2016-01-24 14:49:39 -0500

    Cleaned up test class a bit

--- core/test/java/es/rabbithol/jemblem/BattleCalculatorTest.java
+++ core/test/java/es/rabbithol/jemblem/BattleCalculatorTest.java
@@ -20,46 +20,37 @@ import es.rabbithol.jemblem.model.stats.StatsAdapter;
 public class BattleCalculatorTest {
   private final Engine engine = new Engine();
 
-  private Entity lyn;
-  private Entity eliwood = new Entity();
-  private Entity hector = new Entity();
-
-  @Before
-  public void makeCharacters() {
-    lyn = initLyn();
-  }
+  private final Entity lyn = new CharacterBuilder()
+      .name(new NameComponent("Lyn"))
+      .inventory(new InventoryComponent()
+          .addItem(new WeaponBuilder()
+              .name(new NameComponent("Mani Katti"))
+              .durability(new DurabilityComponent(45))
+              .weaponStats(new WeaponStatsComponent()
+                  .type(WeaponType.SWORD)
+                  .rank(PrfRank.MANI_KATTI)
+                  .might(8)
+                  .accuracy(80)
+                  .crit(20)
+                  .minRange(1)
+                  .maxRange(1)
+                  .weight(3)
+                  .weaponXP(2)
+                  .cost(-1)
+                  .reversesWeaponTriangle(false))
+              .build()
+          )
+      )
+      .position(new PositionComponent(2, 2))
+      .stats(new StatsComponent(FEClasses.LORD_LYN.baseStats()))
+      .proficiency(new WeaponProficiencyComponent()
+          .proficiency(WeaponType.SWORD, StandardRank.E))
+      .build();
+  private final Entity eliwood = new Entity();
+  private final Entity hector = new Entity();
 
   @Test
   public void foo() {
     final BattleCalculator calculator = new BattleCalculator(lyn, eliwood);
-  }
-
-  private Entity initLyn() {
-    return new CharacterBuilder()
-        .name(new NameComponent("Lyn"))
-        .inventory(new InventoryComponent()
-            .addItem(new WeaponBuilder()
-                .name(new NameComponent("Mani Katti"))
-                .durability(new DurabilityComponent(45))
-                .weaponStats(new WeaponStatsComponent()
-                    .type(WeaponType.SWORD)
-                    .rank(PrfRank.MANI_KATTI)
-                    .might(8)
-                    .accuracy(80)
-                    .crit(20)
-                    .minRange(1)
-                    .maxRange(1)
-                    .weight(3)
-                    .weaponXP(2)
-                    .cost(-1)
-                    .reversesWeaponTriangle(false))
-                .build()
-            )
-        )
-        .position(new PositionComponent(2, 2))
-        .stats(new StatsComponent(FEClasses.LORD_LYN.baseStats()))
-        .proficiency(new WeaponProficiencyComponent()
-                .proficiency(WeaponType.SWORD, StandardRank.E))
-        .build();
   }
 }
